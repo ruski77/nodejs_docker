b30ce69dd7abf505298fe62be7d73a9789826784
Russell Adcock <russelladcock@skedulo.com>
2014-09-25 13:07:06 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+// Constants
+var PORT = 8080;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello from inside a Docker container!\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
